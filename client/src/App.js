dd80357ac9417ce9a8abaf06d33ed2773144033f
Lee Seng Kitt <87774484+bobscodedump@users.noreply.github.com>
2023-01-27 13:10:55 +0800

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import React, { Fragment, useState } from "react";
+
+import {BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
+
+//components
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
-  return (
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route exact path="/login" render={props => <Login {...props} />}/>
+            <Route exact path="/register" render={props => <Register {...props} />}/>
+            <Route exact path="/dashboard" render={props => <Dashboard {...props} />}/>
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
